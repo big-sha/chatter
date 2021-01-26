e8471a9b4c56935eef09504fd44497317d4ced98
Sean Maurice Perkins <sean.perkins@rcn.net>
2021-01-26 17:15:08 -0500

    Add CableReady channel

---
+++ app/channels/timeline_channel.rb
@@ -0,0 +1,10 @@
+class TimelineChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from "timeline"
+  end
+
+  # def unsubscribed
+  #   # Any cleanup needed when channel is unsubscribed
+  # end
+end
